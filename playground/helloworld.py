fc986a06232e7e7905712d7f8d93954d34ce8fab
aashishbiradar <aashish.biradar@gmail.com>
2020-08-30 15:30:35 +0530

    hello world

--- playground/helloworld.py
+++ playground/helloworld.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+import logging
+from threading import Thread
+import time
+
+def hello():
+    logging.info('Hello World!')
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    t = Thread(target=hello)
+    t.start()
